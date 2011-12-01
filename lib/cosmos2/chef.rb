f66e293463214c9e9d1ba353974805080b6393f0
Thomas Dudziak <thomas@ning.com>
2011-11-30 18:14:44 -0800

    Added first code for chef integration

---
+++ lib/cosmos2/chef.rb
@@ -0,0 +1,45 @@
+require 'cosmos2'
+require 'cosmos2/plugin'
+
+require_with_hint 'chef/knife', "In order to use the chef plugin please run 'gem install chef'"
+require_with_hint 'chef/node', "In order to use the chef plugin please run 'gem install chef'"
+
+module Cosmos2
+  # A plugin that allows to interact with Chef in cosmos scripts, e.g. to retrieve information about
+  # a server from Ohai or apply a role to a node. You'd typically use it in a cosmos context like so:
+  #
+  #     response = with chef do
+  #       TODO
+  #     end
+  #
+  # Note that this plugin will not actually talk to Chef in dry-run mode.
+  # Instead it will only send messages tagged as `:chef` and `:dryrun`.
+  class Chef < Plugin
+    # The plugin's configuration
+    attr_reader :config
+
+    # Creates a new chef plugin instance.
+    #
+    # @param [Environment] environment The cosmos environment
+    # @param [Symbol] name The name for this plugin instance e.g. in the config
+    # @return [Chef] The new instance
+    def initialize(environment, name = :chef)
+      @environment = environment
+      @config = @environment.get_plugin_config(:name => name.to_sym)
+      @connections = {}
+      @knife = Chef::Knife.new
+      @knife.config[:verbosity] = 0 # Only error logging for now, TODO configure a cosmos logger
+      @knife.configure_chef
+    end
+
+    # Retrieves the info that Chef has for a given host.
+    #
+    # @param [Hash] params The parameters
+    # @option params [String] :host The host to get the info for
+    # @return [Hash,nil] A hash with the info for the host
+    def get_info(params)
+      host = params[:host] or raise "No :host argument given"
+      Chef::Node.load(host)
+    end
+  end
+end
