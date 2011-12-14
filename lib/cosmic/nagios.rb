aecf3056172c636935bbb1ec9ad3d8006b8e75d1
Thomas Dudziak <thomas@ning.com>
2011-12-14 13:22:57 -0800

    First version of a nagios plugin

---
+++ lib/cosmic/nagios.rb
@@ -0,0 +1,104 @@
+require 'cosmic'
+require 'cosmic/plugin'
+require_with_hint 'nagix', "In order to use the nagios plugin please run 'gem install nagix'"
+require_with_hint 'json', "In order to use the nagios plugin please run 'gem install json'"
+
+module Cosmic
+  # A plugin that allows Cosmic scripts to control Nagios, for instance to turn off Nagios checks
+  # for hosts while updating them:
+  #
+  #     with nagios do
+  #       disable :host => host
+  #       # do something with the host
+  #       enable :host => host
+  #     end
+  #
+  # The plugin needs to be configured with the url of the [Nagix](https://github.com/ning/Nagix)
+  # server.
+  #
+  # Note that this plugin will not actually perform any destructive actions in Nagios in dry-run
+  # mode. Instead it will only send messages tagged as `:nagios` and `:dryrun`.
+  class Nagios < Plugin
+    # The plugin's configuration
+    attr_reader :config
+
+    # Creates a new nagios plugin instance.
+    #
+    # @param [Environment] environment The Cosmic environment
+    # @param [Symbol] name The name for this plugin instance e.g. in the config
+    # @return [Nagios] The new instance
+    def initialize(environment, name = :nagios)
+      @environment = environment
+      @config = @environment.get_plugin_config(:name => name.to_sym)
+      @environment.resolve_service_auth(:service_name => name.to_sym, :config => @config)
+      @nagix = Net::HTTP.new(@config[:nagix_host])
+    end
+
+    # Returns the Nagios status for the given host. This is equivalent to the MK Livestatus
+    # returned by Nagios.
+    #
+    # @param [Hash] params The parameters
+    # @option params [String] :host The host to return the status for
+    # @option params [String,nil] :service The service on the host to return the status for;
+    #                                      if not specified, then the status of the host will
+    #                                      be returned
+    # @return [Hash] The Nagios status of the host/service as a Hash
+    def status(params)
+      host = params[:host] or raise "No :host argument given"
+      service = params[:service]
+      if service
+        request = Net::HTTP::Get.new("/hosts/#{host}/#{service}/attributes?format=json")
+      else
+        request = Net::HTTP::Get.new("/hosts/#{host}/attributes?format=json")
+      end
+      response = @nagix.request(request)
+      JSON.parse(response.body)
+    end
+
+    # Enables all configured Nagios checks for the given host. This is equivalent
+    # to the `ENABLE_HOST_SVC_NOTIFICATIONS` and `ENABLE_SVC_NOTIFICATIONS` Nagios
+    # commands.
+    #
+    # @param [Hash] params The parameters
+    # @option params [String] :host The host to enable checks for
+    # @option params [String,nil] :service The service on the host to enable checks for;
+    #                                      if not specified then all services will be enabled
+    # @return [Hash] The Nagios status of the host/service as a Hash
+    def enable(params)
+      host = params[:host] or raise "No :host argument given"
+      service = params[:service]
+      if service
+        request = Net::HTTP::Put.new("/hosts/#{host}/#{service}/command/ENABLE_SVC_NOTIFICATIONS")
+      else
+        request = Net::HTTP::Put.new("/hosts/#{host}/command/ENABLE_SVC_NOTIFICATIONS")
+      end
+      request.body = "{}"
+      request["Content-Type"] = "application/json"
+      @nagix.request(request)
+      status(params)
+    end
+
+    # Enables all configured Nagios checks for the given host. This is equivalent
+    # to the `DISABLE_HOST_SVC_NOTIFICATIONS` and `DISABLE_SVC_NOTIFICATIONS` Nagios
+    # commands.
+    #
+    # @param [Hash] params The parameters
+    # @option params [String] :host The host to disable checks for
+    # @option params [String,nil] :service The service on the host to disable checks for;
+    #                                      if not specified then all services will be disabled
+    # @return [Hash] The Nagios status of the host/service as a Hash
+    def disable(params)
+      host = params[:host] or raise "No :host argument given"
+      service = params[:service]
+      if service
+        request = Net::HTTP::Put.new("/hosts/#{host}/#{service}/command/DISABLE_SVC_NOTIFICATIONS")
+      else
+        request = Net::HTTP::Put.new("/hosts/#{host}/command/DISABLE_SVC_NOTIFICATIONS")
+      end
+      request.body = "{}"
+      request["Content-Type"] = "application/json"
+      @nagix.request(request)
+      status(params)
+    end
+  end
+end
